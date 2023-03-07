62231e9e7a6d4f675735c3a2023480a112c1f369
Jiji <102505581+SeulbiChoe@users.noreply.github.com>
2023-03-07 11:13:00 +0900

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".popup_content .swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 10,
+});
