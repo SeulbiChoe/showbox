1dc9ff5824a74736e151675e462d38bccc0a24ed
Jiji <102505581+SeulbiChoe@users.noreply.github.com>
2023-03-16 17:35:18 +0900

    Delete swiper.js

--- js/swiper.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".popup_content .swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  slidesPerView: 'auto',
-  centeredSlides: true,
-  spaceBetween: 10,
-});
